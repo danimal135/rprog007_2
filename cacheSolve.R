03d0524beb056865591530951e4f49380ce6b8c7
Dan Northrup <danimal135@gmail.com>
2014-09-20 16:05:51 -0400

    all files to commit

---
+++ cacheSolve.R
@@ -0,0 +1,11 @@
+cachemat <- function(x, ...) {
+  m <- x$getmat()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmean(m)
+  m
+}
---
+++ makeCacheMatrix.R
@@ -0,0 +1,13 @@
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmat <- function(solve) m <<- solve
+  getmat <- function() m
+  list(set = set, get = get,
+       setmat = setmat,
+       getmat = getmat)
+}
